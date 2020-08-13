a3cd83db7f37dea35910d3f6617d5f7011f7da6f
Guerric Phalippou <guerric.phalippou@gadz.org>
2020-08-13 10:02:48 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cf api "$INPUT_CF_API"
 cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
-cf install-plugin multiapps -y
+cf install-plugin multiapps -f
 
 if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
   cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
