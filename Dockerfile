4c07d283b6325dbadcce8dc0117f233c918ecbb3
Tse Eche <tse.eche@gmail.com>
2023-11-16 14:06:05 +0100

    fix component versions to ensure repeatable builds

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM ubuntu:18.04
+FROM ubuntu:mantic-20231011
 
 RUN apt-get update
-RUN apt-get install -y ca-certificates jq
+RUN apt-get install -y ca-certificates jq wget
 
 RUN echo "deb [trusted=yes] https://packages.cloudfoundry.org/debian stable main" > /etc/apt/sources.list.d/cloudfoundry-cli.list
 RUN apt-get update
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,15 @@
 
 cf api "$INPUT_CF_API"
 cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
-cf install-plugin multiapps -f
+
+# This would install latest available --> moving target, potential issues
+# cf install-plugin multiapps -f
+
+# Safer: download a specific version of the plugin known to work with chosen ubuntu version
+wget --quiet -O multiapps-plugin-v3.2.1.linux64 https://github.com/cloudfoundry/multiapps-cli-plugin/releases/download/v3.2.1/multiapps-plugin.linux64
+
+# Install the plugin from the local file
+cf install-plugin -f multiapps-plugin-v3.2.1.linux64
 
 if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
   cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
